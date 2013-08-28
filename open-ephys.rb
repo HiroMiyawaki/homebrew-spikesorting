6de88eaaed4fdfdf69e1a65e6022b326a3a0d44a
Florian Franzen <FlorianFranzen@gmail.com>
2013-08-28 18:13:40 +0200

    open-ephys: Fixed deployment path for .app folder and added caveats info about how to install the app.

--- open-ephys.rb
+++ open-ephys.rb
@@ -2,18 +2,33 @@ require 'formula'
 
 class OpenEphys < Formula
   homepage 'http://open-ephys.org/#/gui/'
-  head 'https://github.com/open-ephys/GUI.git', :using => :git
   url  'https://github.com/open-ephys/GUI/archive/v0.1.tar.gz'
   sha1 '903d19349ee09e7cbaf4ea389ee4f8845a7c3aca'
+
+  head 'https://github.com/open-ephys/GUI.git', :using => :git
 
   depends_on :xcode => :build
   depends_on 'libftdi'
 
   def install
-    system "xcodebuild install DSTROOT=#{prefix} SYMROOT=build OBJROOT=objroot -project Builds/MacOSX/open-ephys.xcodeproj -target open-ephys -configuration Release"
+    system "xcodebuild install DSTROOT=#{prefix} INSTALL_PATH=/ SYMROOT=build OBJROOT=objroot -project Builds/MacOSX/open-ephys.xcodeproj -target open-ephys -configuration Release"
+  end
+
+   def caveats; <<-EOS.undent
+      open-ephys is an gui only application.
+
+      To install the Mac OS X wrapper application run:
+        brew linkapps [--system]
+      
+      If the optional '--system' flag is not set, the wrapper 
+      is installed into '~/Applications/' instead of '/Applications/'.
+
+      Alternativly you can also run:
+        ln -s #{opt_prefix}/open-ephys.app /Applications
+    EOS
   end
 
   test do
-    system "open open-ephys"
+    system "open #{prefix}/open-ephys.app"
   end
 end
