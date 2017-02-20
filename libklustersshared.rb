8ca3b64ca0c923c5b4783800863d4539a6c5eccb
HiroMiyawaki <miyawaki@dhcp-sch-f15-223-54.med.osaka-cu.ac.jp>
2017-02-20 10:00:20 +0900

    QWeb to QWebEngine

--- libklustersshared.rb
+++ libklustersshared.rb
@@ -24,11 +24,12 @@ class Libklustersshared < Formula
       ENV.append "CXXFLAGS", "-g -O2"
     end
 
+    system "cmake -DENFORCE_QT4_BUILD=OFF", ".", *std_cmake_args
+
     inreplace 'src/gui/makefile' do |s|
       s.gsub! 'QWeb', 'QWebEngine' 
     end    
 
-    system "cmake -DENFORCE_QT4_BUILD=OFF", ".", *std_cmake_args
     system "make install"
   end
 end
