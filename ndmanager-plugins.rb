5b1e264fc43d31bffa94fa079b162fccb0a58640
HiroMiyawaki <miyawaki@dhcp-sch-f15-223-54.med.osaka-cu.ac.jp>
2017-02-22 18:34:49 +0900

    exclude python dir

--- ndmanager-plugins.rb
+++ ndmanager-plugins.rb
@@ -40,7 +40,7 @@ class NdmanagerPlugins < Formula
       s.gsub! '/usr/bin/pyrcc4', "#{HOMEBREW_PREFIX}/bin/pyrcc5" 
     end
 
-   inreplace 'python/ndm_prepare/makefile' do |s|
+   inreplace 'makefile' do |s|
       s.gsub! '@echo "Making all in $(PYTHON_DIR)..."; (cd $(PYTHON_DIR); $(MAKE));', ''
       s.gsub! '@(cd $(PYTHON_DIR)$$i; $(MAKE) clean);', ''
       s.gsub! '@(cd $(PYTHON_DIR); $(MAKE) install);', ''
